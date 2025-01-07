f3ce17d6ae46854d4e0f5c34f0b1630cf8920e15
Jarvis-9 <jayanthvunnam7@gmail.com>
2025-01-06 18:50:57 -0700

    Added new for js

--- script.js
+++ script.js
@@ -46,22 +46,62 @@ function playRound(humanChoice, ComputerChoice){
     //console.log(ComputerScore, humanScore)
 }
 
-function playGame(){
-    for(let i = 0; i<5; i++){
-        let humanChoice = getHumanChoice()
-        let ComputerChoice = getComputerChoice()
-        playRound(humanChoice, ComputerChoice)
-    }
-    if(humanScore == ComputerScore){
-        alert('Tie Game')
-    }
-    else if(humanScore>ComputerScore){
-        alert('Human beats Computer')
-    }
-    else{
-        alert('Computer beats Human')
-    }
-    console.log('Computer Score: '+ ComputerScore+ ' HumanScore: '+humanScore)
-}
+// function playGame(){
+//     for(let i = 0; i<5; i++){
+//         let humanChoice = getHumanChoice()
+//         let ComputerChoice = getComputerChoice()
+//         playRound(humanChoice, ComputerChoice)
+//     }
+//     if(humanScore == ComputerScore){
+//         alert('Tie Game')
+//     }
+//     else if(humanScore>ComputerScore){
+//         alert('Human beats Computer')
+//     }
+//     else{
+//         alert('Computer beats Human')
+//     }
+//     console.log('Computer Score: '+ ComputerScore+ ' HumanScore: '+humanScore)
+// }
 
-playGame()
+// playGame()
+// let humanChoice = getHumanChoice()
+let ComputerChoice = getComputerChoice()
+// playRound(humanChoice, ComputerChoice)
+
+let container = document.querySelector('#container')
+let div = document.createElement('div')
+let p = document.createElement('p')
+let p1 = document.createElement('p')
+
+
+div.appendChild(p)
+div.appendChild(p1)
+container.appendChild(div)
+
+let btn1 = document.querySelector('#rock')
+let btn2 = document.querySelector('#paper')
+let btn3 = document.querySelector('#scissor')
+
+btn1.addEventListener('click', ()=>{
+    playRound('rock', getComputerChoice())
+    showScore()
+})
+btn2.addEventListener('click', ()=>{
+    playRound('paper', getComputerChoice())
+    showScore()
+})
+btn3.addEventListener('click', ()=>{
+    playRound('scissor', getComputerChoice())
+    showScore()
+})
+
+function showScore(){
+    p.textContent = `Human Score: ${humanScore} Computer Score: ${ComputerScore}`
+    if(humanScore>=5){
+        p1.textContent =  `Human is the Winner`
+    }
+    else if(ComputerScore>=5){
+        p1.textContent =   `Computer is the Winner`
+    }
+}
